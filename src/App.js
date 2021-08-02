e8323ac04a30512b93ecdf6da4582d5a21fc529c
Manmeet Singh <manmeetsvirdi99@gmail.com>
2021-08-02 04:21:12 -0700

    myfirst commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react'
 // import UseState from './components/Hooks/UseState';
 // import UseEffect from './components/Hooks/useEffect';
 // import UseReducer from './components/Hooks/UseReducer';
-import Todo from './components/todoreact/Todo';
+// import Todo from './components/todoreact/Todo';
+import Temp from './components/Weather/Temp';
+
 
 const App = () => {
   return (
     <React.Fragment>
-      <Todo></Todo>
+      <Temp/>
+    
     </React.Fragment>
   )
 }
